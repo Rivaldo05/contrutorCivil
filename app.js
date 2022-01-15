f993e35f63f829b784ff2d4ea5b85dadacb2440e
Rivaldo05 <67082413+Rivaldo05@users.noreply.github.com>
2022-01-15 15:47:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// função loaddash pega da internet
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+};
+
+const buscarElemento = document.querySelectorAll('[data-animeCard]')
+const animationClass = 'animate'
+
+function animeScrool(){
+    const windowTop = window.scrollY + ((window.innerHeight * 3) / 3)
+    buscarElemento.forEach((parametro)=>{
+        if(windowTop > parametro.offsetTop){
+            parametro.classList.add(animationClass)
+        }else{
+            parametro.classList.remove(animationClass)
+        }
+    })
+}
+
+/* sem debouce */
+animeScrool()
+if(buscarElemento.length){
+    window.addEventListener('scroll',debounce(()=>{
+    animeScrool()
+},200)) 
+}
+
+/**com Debounce */
+animeScrool()
+if(buscarElemento.length){
+    window.addEventListener('scroll', ()=>{
+    animeScrool()
+})
+}
